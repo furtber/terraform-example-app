6874ebcd373b7a64aa4dac1e2dbad228793f4429
Jukka Forsgren <forjukka@amazon.com>
2019-02-28 08:14:22 +0100

    Added a remark.

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "terraform-tfstate-825265825471"
         workspace_key_prefix = "workspace"
-        key = "example-app.tfstate"
+        key = "example-app.tfstate" # make sure to change this if you copy-paste this stanza
         region = "eu-west-1"
         dynamodb_table = "TerraformStateLock"
         encrypt = "1"
